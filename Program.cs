808a471fc20bac734c13afa83f6327c0101ee473
Bencso <158219939+bencso@users.noreply.github.com>
2024-10-07 10:35:05 +0200

    Upload my solution

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using babolnai_bence_triatlon;
+using System.IO;
+using System.Text;
+
+List<Versenyzo> versenyzok = new();
+StreamReader sr = new StreamReader("@/../../../../src/forras.txt", encoding: Encoding.UTF8);
+while (!sr.EndOfStream) versenyzok.Add(new Versenyzo(sr.ReadLine()!));
+
+Console.WriteLine("Ennyien fejezték be a versenyt: "+versenyzok.Count.ToString());
+Console.WriteLine("Ennyi elit kategoriában versenyzők száma: "+versenyzok.Count(p=>p.Kategoria.ToLower()=="elit"));
+Console.WriteLine("Női versenyzők álatéletkora: " + versenyzok.Where(p => p.Nem == "n").Average(p => DateTime.Now.Year - p.SzuletesiEv) + " év.");
+Console.WriteLine("Kerékpározás összideje: "+Math.Round(TimeSpan.FromSeconds(versenyzok.Sum(p => p.KerekparozasIdo.TotalSeconds)).TotalHours,2));
+Console.WriteLine("Úszás átlagos idejű elit junior kategória: " + TimeSpan.FromSeconds(versenyzok.Average(p => p.UszasiIdo.TotalSeconds)));
+Console.WriteLine("Férfi győztes ideje: " + versenyzok.Min(p => TimeSpan.FromSeconds(p.KerekparozasIdo.TotalSeconds + p.UszasiIdo.TotalSeconds + p.FutasIdo.TotalSeconds)));
+var kategoriak = versenyzok.GroupBy(p => p.Kategoria).Distinct().OrderBy(p=>p.Key);
+Console.WriteLine("Korkategóriánként versenyt befejezők száma:");
+foreach (var item in kategoriak)
+{
+    Console.WriteLine($"\t{item.Key.Substring(0, 1).ToUpper() + item.Key.Substring(1)}: {item.Count()} db");
+}
+Console.WriteLine("\nKorkategóriánként átlag depóidő:");
+foreach (var item in kategoriak)
+{
+    Console.WriteLine($"\t{item.Key.Substring(0, 1).ToUpper() + item.Key.Substring(1)}: {TimeSpan.FromSeconds(item.Average(p=>item.Sum(p => p.IDepoIdo.TotalSeconds + p.IIDepoIdo.TotalSeconds)))}");
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babolnai_bence_triatlon
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int SzuletesiEv { get; set; }
+        public int VersenySzam { get; set; }
+
+        public string Nem { get; set; }
+        public string Kategoria { get; set; }
+        public TimeSpan UszasiIdo { get; set; }
+        public TimeSpan IDepoIdo { get; set; }
+        public TimeSpan KerekparozasIdo { get; set; }
+        public TimeSpan IIDepoIdo { get; set; }
+        public TimeSpan FutasIdo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nev} ({Nem}) - {SzuletesiEv} {Kategoria}";
+        }
+
+        public Versenyzo(string sor)
+        {
+            string[] db = sor.Split(";");
+            Nev = db[0];
+            SzuletesiEv = int.Parse(db[1]);
+            VersenySzam = int.Parse(db[2]);
+            Nem = db[3];
+            Kategoria = db[4];
+            UszasiIdo = TimeSpan.Parse(db[5]);
+            IDepoIdo = TimeSpan.Parse(db[6]);
+            KerekparozasIdo = TimeSpan.Parse(db[7]);
+            IIDepoIdo = TimeSpan.Parse(db[8]);
+            FutasIdo = TimeSpan.Parse(db[9]);
+        }
+    }
+
+
+}
